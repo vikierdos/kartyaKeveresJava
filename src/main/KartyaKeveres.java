2a153772854676fa06861dacfee714c11d52c7ff
ErdősViktória(SZF_N_ <ErdsViktria(SZF_N_@Tanulo-305-4.oktatas.local>
2026-09-16 16:28:52 +0200

    kartyakeveres elkezdese, tombok, metodusok alap

---
+++ src/main/KartyaKeveres.java
@@ -0,0 +1,36 @@
+
+package main;
+
+public class KartyaKeveres {
+
+    public static void main(String[] args) {
+        
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] kartya = {"Ász","Kir","Fel","X","XI","XII","XIII"};
+        
+        for (int i = 0; i < 3; i++) {
+            kirak();
+            melyik();
+            kever();
+        }
+        ezVolt();
+        
+    }
+
+    private static void kirak() {
+        
+    }
+
+    private static void melyik() {
+        
+    }
+
+    private static void kever() {
+        
+    }
+
+    private static void ezVolt() {
+        
+    }
+    
+}
