8edf483f405786690208f3c5ae5f65fea4158fce
ErdősViktória(SZF_N_ <ErdsViktria(SZF_N_@Tanulo-305-4.oktatas.local>
2026-09-16 16:53:14 +0200

    feltolt metodus kesz

--- src/main/KartyaKeveres.java
+++ src/main/KartyaKeveres.java
@@ -1,12 +1,20 @@
 
 package main;
 
-public class KartyaKeveres {
+import java.util.Scanner;
 
+public class KartyaKeveres {
+    
+    static Scanner beolv = new Scanner(System.in);  
+    static String[] pakli = new String[22];
+    
     public static void main(String[] args) {
+        feladat();
+    }
+
+    private static void feladat() {
+        feltolt();
         
-        String[] szinek = {"P", "T", "Z", "M"};
-        String[] kartya = {"Ász","Kir","Fel","X","XI","XII","XIII"};
         
         for (int i = 0; i < 3; i++) {
             kirak();
@@ -14,7 +22,20 @@ public class KartyaKeveres {
             kever();
         }
         ezVolt();
-        
+    }
+    
+    private static void feltolt() {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász","Kir","Fel","X","IX","VIII"};
+        int db = 0;
+        for (String szin : szinek) {
+            for (String ertek : ertekek) {
+                String kartya = szin + "_" + ertek;
+                if (db < 21){
+                    pakli[++db] = kartya;
+                }
+            }
+        }
     }
 
     private static void kirak() {
@@ -22,7 +43,7 @@ public class KartyaKeveres {
     }
 
     private static void melyik() {
-        
+        int oszlop = beolv.nextInt();
     }
 
     private static void kever() {
