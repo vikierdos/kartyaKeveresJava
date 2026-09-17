2c3ac6276e44e192381827ce96f65f97d6cad18f
ErdősViktória(SZF_N_ <ErdsViktria(SZF_N_@Tanulo-305-4.oktatas.local>
2026-09-17 14:56:48 +0200

    keveres kesz

--- src/main/KartyaKeveres.java
+++ src/main/KartyaKeveres.java
@@ -18,8 +18,8 @@ public class KartyaKeveres {
         
         for (int i = 0; i < 3; i++) {
             kirak();
-            melyik();
-            kever();
+            int o = melyik();
+            kever(o);
         }
         ezVolt();
     }
@@ -39,19 +39,60 @@ public class KartyaKeveres {
     }
 
     private static void kirak() {
-        
+        System.out.printf("%-10s%-10s%-10s\n", "1. o", "2. o", "3. o");
+        for (int i = 1; i < pakli.length; i++) {
+            System.out.printf("%-10s", pakli[i]);
+            if(i % 3 == 0){
+                System.out.println("");
+            }
+        }
     }
 
-    private static void melyik() {
+    private static int melyik() {
+        System.out.print("Melyik oszlop? (1-3): \n");
         int oszlop = beolv.nextInt();
+        while(oszlop < 1 || oszlop > 3){
+            System.out.print("Hibás, újra: \n");
+            oszlop = beolv.nextInt();
+        }
+        
+        return oszlop;
+        
     }
 
-    private static void kever() {
+    private static void kever(int oszlop) {
+        String[] ujPakli = new String[22];
         
+        switch(oszlop){
+            case 1 -> {
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[20-(i-1)*3];
+                    ujPakli[i+7] = pakli[19-(i-1)*3];
+                    ujPakli[i+14] = pakli[21-(i-1)*3];
+                }
+            }
+            case 2 -> {
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19-(i-1)*3];
+                    ujPakli[i+7] = pakli[20-(i-1)*3];
+                    ujPakli[i+14] = pakli[21-(i-1)*3];
+                }
+            }
+            
+            case 3 -> {
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19-(i-1)*3];
+                    ujPakli[i+7] = pakli[21-(i-1)*3];
+                    ujPakli[i+14] = pakli[20-(i-1)*3];
+                }
+            }
+        }
+        
+        pakli = ujPakli;
     }
 
     private static void ezVolt() {
-        
+        System.out.println("Ez volt a kártyád: " + pakli[11]);
     }
     
 }
